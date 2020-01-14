b9200e1f19a67ca9c60cb7c2a9cfd65d2ddccec6
Dhanendra Kumar Verma <v.dhanendra@gmail.com>
2020-01-14 23:54:10 +0530

    post and comment model

---
+++ mysite/blog/froms.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlreslovers import reverse
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comments = models.BooleanField(default=False)
