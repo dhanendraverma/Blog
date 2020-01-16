b5f0e8aff6436d799db51c1af9208476a8e3447b
Dhanendra Kumar Verma <v.dhanendra@gmail.com>
2020-01-16 16:54:37 +0530

    removing the errors

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,29 +1,36 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urlreslovers import reverse
+from django.urls import reverse
+
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
-    published_date = models.DateTimeField(blank=True,null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-    def approved_comments(self):
+    def approve_comments(self):
         return self.comments.filter(approved_comment=True)
+
+    def get_absolute_url(self):
+        return reverse("post_detail",kwargs={'pk':self.pk})
+
 
     def __str__(self):
         return self.title
 
+
+
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post',on_delete=models.DO_NOTHING,related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
@@ -31,7 +38,7 @@ class Comment(models.Model):
         self.save()
 
     def get_absolute_url(self):
-        return reverse('post_list')
-    
+        return reverse("post_list")
+
     def __str__(self):
         return self.text
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('account/login/',views.login,name=login),
-    path('account/logout/',views.logout,kwargs={'next_page':'/'}),
+    path('account/login/',views.LoginView,name='login'),
+    path('account/logout/',views.LogoutView,kwargs={'next_page':'/'}),
 ]
